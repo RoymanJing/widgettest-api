6fdee4d1d35359e61c652afec15ed81b86248e22
Royman Jing <feishu1979@163.com>
2017-09-11 14:53:56 -0700

    fix socket hang up

--- index.js
+++ index.js
@@ -10,26 +10,32 @@ app.use("/",router);
 router.get("/widget/header",function(req, res){
   console.log(req.url)
   res.sendFile(path + "header.html");
+  res.end();
 });
 router.get("/widget/footer",function(req, res){
   console.log(req.url)
   res.sendFile(path + "footer.html");
+  res.end();
 });
 router.get("/widget/center",function(req, res){
   console.log(req.url)
   res.sendFile(path + "center.html");
+  res.end();
 });
 router.get("/widget/leftsider",function(req, res){
   console.log(req.url)
   res.sendFile(path + "leftsider.html");
+  res.end();
 });
 router.get("/widget/rightsider",function(req, res){
   console.log(req.url)
   res.sendFile(path + "rightsider.html");
+  res.end();
 });
   
 app.use("*",function(req, res){
   res.send("Error 404: Not Found!");
+  res.end();
 });
   
 app.listen(3001,function(){
